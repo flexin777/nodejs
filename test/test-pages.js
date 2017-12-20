52e13f052ea7fcf648e5933aee4e0af3f38b3501
Vadym Morozov <vadym.morozov89@gmail.com>
2017-12-20 18:03:01 +0200

    added execution of shell script.

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,9 +1,25 @@
 var expect  = require('chai').expect;
 var request = require('request');
 
+function check(done) {
+request('http://localhost:8080/hello' , function(error, response, body) {
+       expect(body).to.equal('<html><body><p>world.</p></body></html>');
+       done();
+   });
+
+}
+
 it('Main page content', function(done) {
-    request('http://localhost:8080/hello' , function(error, response, body) {
-        expect(body).to.equal('<html><body><p>world.</p></body></html>');
-        done();
-    });
-});
+var exec = require('child_process').exec, child;
+
+exec('node /home/vadym/Desktop/nodejs/nodejs-master/index.js',
+   function (error, stdout, stderr) {
+       console.log('stdout: ' + stdout);
+       console.log('stderr: ' + stderr);
+    check();
+       if (error !== null) {
+            console.log('exec error: ' + error);
+       }
+   });
+done();
+   });
